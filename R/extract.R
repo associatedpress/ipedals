e290accf56a2ff24e3e86ba47a9d8a7ec3c3e778
Justin Myers <jmyers@ap.org>
2020-04-02 16:25:38 -0500

    Correct assorted `devtools::check` notes/warnings

--- R/extract.R
+++ R/extract.R
@@ -5,7 +5,8 @@
 #' @param column_types A column specification as returned by `readr::cols`.
 #' @return A tibble of data.
 extract_csv <- function(local_path, column_types) {
-  csv_filenames <- unzip(local_path, list = TRUE) %>%
+  Name = lazyeval::as_name("Name")
+  csv_filenames <- utils::unzip(local_path, list = TRUE) %>%
     tibble::as_tibble() %>%
     dplyr::filter(stringr::str_detect(Name, "\\.csv$"))
   csv_filename <- csv_filenames[1,]$Name
@@ -17,14 +18,17 @@ extract_csv <- function(local_path, column_types) {
 #'
 #' @importFrom magrittr %>%
 #' @param local_path The path to a downloaded zip file.
+#' @param sheet_name Name of the sheet to extract from the Excel file contained
+#'   in the zip.
 #' @return A tibble of data.
 extract_metadata_sheet <- function(local_path, sheet_name) {
-  xlsx_filenames <- unzip(local_path, list = TRUE) %>%
+  Name = lazyeval::as_name("Name")
+  xlsx_filenames <- utils::unzip(local_path, list = TRUE) %>%
     tibble::as_tibble() %>%
     dplyr::filter(stringr::str_detect(Name, "\\.xlsx$"))
   xlsx_filename <- xlsx_filenames[1,]$Name
   local_dir <- dirname(local_path)
-  unzip(local_path, xlsx_filename, exdir = local_dir)
+  utils::unzip(local_path, xlsx_filename, exdir = local_dir)
   readxl::read_excel(file.path(local_dir, xlsx_filename), sheet = sheet_name)
 }
 
